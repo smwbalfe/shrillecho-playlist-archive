53a3d8e7aa7cddbfb6d1e475fb1c569d2c7f368b
smwbalfe <smwbalfe@gmail.com>
2025-02-15 19:05:54 +0000

    persist state

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,54 +1,52 @@
 terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket = "shrillecho-tf-state"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
-  }
+ required_providers {
+   digitalocean = {
+     source  = "digitalocean/digitalocean"
+     version = "~> 2.0"
+   }
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 5.0"
+   }
+ }
 }
 
 provider "digitalocean" {
-  token = var.do_token
+ token = var.do_token
 }
 
 provider "aws" {
-  region = "eu-west-2"
+ region = "eu-west-2"
 }
-
 
 variable "do_token" {
-  description = "DigitalOcean API Token"
-  type        = string
-  sensitive   = true
-}
-
-resource "digitalocean_droplet" "web" {
-  name   = "web-1"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-22-04-x64"
-  tags   = ["web", "terraform"]
-  ssh_keys = ["5f:19:30:49:c4:47:17:81:e7:c6:88:5d:ac:01:65:b2"]
+ description = "DigitalOcean API Token"
+ type        = string
+ sensitive   = true
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "shrillecho-tf-state"  
+ bucket = "shrillecho-tf-state"
 }
 
+resource "digitalocean_droplet" "web" {
+ name     = "web-1"
+ region   = "nyc1"
+ size     = "s-1vcpu-1gb"
+ image    = "ubuntu-22-04-x64"
+ tags     = ["web", "terraform"]
+ ssh_keys = ["5f:19:30:49:c4:47:17:81:e7:c6:88:5d:ac:01:65:b2"]
+}
 
 output "droplet_ip" {
-  value = digitalocean_droplet.web.ipv4_address
+ value = digitalocean_droplet.web.ipv4_address
+}
+
+terraform {
+ backend "s3" {
+   bucket  = "shrillecho-tf-state"
+   key     = "terraform.tfstate"
+   region  = "eu-west-2"
+   encrypt = true
+ }
 }
