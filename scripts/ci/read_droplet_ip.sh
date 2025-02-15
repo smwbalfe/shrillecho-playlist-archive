be8f7bda3c15b15f6c46d11845d91f0537401ca5
smwbalfe <smwbalfe@gmail.com>
2025-02-15 20:27:06 +0000

    persist state

--- scripts/ci/read_droplet_ip.sh
+++ scripts/ci/read_droplet_ip.sh
@@ -2,5 +2,5 @@
 
 STATE_FILE="s3://${BUCKET_NAME}/terraform.tfstate"
 DROPLET_IP=$(aws s3 cp "${STATE_FILE}" - | jq -r '.resources[] | select(.type == "digitalocean_droplet") | .instances[].attributes.ipv4_address')
-echo $DROPLET_IP
+echo "droplet_ip=${DROPLET_IP}"
 echo "droplet_ip=${DROPLET_IP}" >> $GITHUB_OUTPUT
