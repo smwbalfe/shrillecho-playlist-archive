f5983b5f8170e50bcc5c47e7cf6d8ea99232e931
smwbalfe <smwbalfe@gmail.com>
2025-02-17 00:02:38 +0000

    remove worker

--- worker/load_env.ps1
+++
@@ -1,9 +0,0 @@
-Get-Content .env | ForEach-Object {
-    if ($_ -match '^\s*([^#][^=]+)=(.*)$') {
-        $name = $Matches[1].Trim()
-        $value = $Matches[2].Trim()
-        if ($name -and $value) {
-            Set-Item -Path "env:$name" -Value $value
-        }
-    }
-}
