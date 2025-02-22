a433dac2f860b73014e63efd99743fe5edff69b5
smwbalfe <smwbalfe@gmail.com>
2025-02-22 14:40:33 +0000

    devops update

--- backend/internal/config/environment.go
+++ backend/internal/config/environment.go
@@ -30,11 +30,20 @@ func LoadEnv() Environment {
 	}
 
 	return Environment{
-		RedisHost:      "104.248.163.15",
-		RedisPort:      "6379",
-		PostgresHost:   "aws-0-eu-west-2.pooler.supabase.com:6543",
-		AllowedOrigins: [] string{"http://localhost:3000", "http://localhost:3000/"},
-		ServerHost:     "localhost",
-		ServerPort:     "8000",
-	}
+			RedisHost:      "redis",
+			RedisPort:      "6379",
+			PostgresHost:   "db",
+			AllowedOrigins: [] string{"https://shrillecho.app"},
+			ServerHost:     "",
+			ServerPort:     "8000",
+		}
+
+	// return Environment{
+	// 	RedisHost:      "104.248.163.15",
+	// 	RedisPort:      "6379",
+	// 	PostgresHost:   "aws-0-eu-west-2.pooler.supabase.com:6543",
+	// 	AllowedOrigins: [] string{"http://localhost:3000", "http://localhost:3000/"},
+	// 	ServerHost:     "localhost",
+	// 	ServerPort:     "8000",
+	// }
 }
